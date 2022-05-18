a041aca2da9131e31ff8d307e9af56b726a99b45
Bartosz Trepka <bartosz.trepka@gmail.com>
2022-05-18 17:06:29 +0200

    Update index.html, style.css, script.js - New grid button

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
+function createGrid(counter) {
+    for (let i = 0; i < counter; i++) {
+        for (let j = 0; j < counter; j++) {
+            appendString += `<div class='box' style="width: ${100/counter}%"></div>`;
+        }
+    }
+    container.innerHTML = appendString;
+    appendString = "";
+
+    addHover();
+}
+
+function addHover() {
+    for (let i = 0; i < gridList.length; i++) {
+        gridList[i].addEventListener('mouseenter', () => {
+            gridList[i].classList += ' hovered';
+        });
+    }
+}
+
 const container = document.getElementById('container');
 const gridList = container.getElementsByTagName('div');
+const newGridButton = document.getElementById('new-grid');
 
 let appendString = "";
 let counter = 16;
 
-for (let i = 0; i < counter; i++) {
-    for (let j = 0; j < counter; j++) {
-        appendString += '<div class="box"></div>';
-    }
-}
-container.innerHTML = appendString;
+createGrid(counter);
 
-for (let i = 0; i < gridList.length; i++) {
-    gridList[i].addEventListener('mouseenter', () => {
-        gridList[i].classList += ' hovered';
-    });
-}
+newGridButton.addEventListener('click', () => {
+    counter = prompt('Insert new grid size:', parseInt(16));
+
+    while (counter > 100 || counter < 1 || isNaN(counter)) {
+        counter = prompt('Insert new grid size:', 16);
+    }
+
+    container.innerHTML = "";
+    createGrid(counter);
+});
+
+//style='width: ${counter}vw; height: ${counter}vw
